b193ef9146aa88c5a6a4c45ea9648e3daca243a5
derdem <nikodem.bienia@gmail.com>
2020-06-07 22:04:41 +0200

    easy twitter api - show tump tweets

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import tweepy
-from .consumerData import consumer_key, consumer_secret
+from twitterCredentials import consumer_key, consumer_secret, access_token, access_token_secret
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+
+for tweet in public_tweets:
+    print(tweet.text)
